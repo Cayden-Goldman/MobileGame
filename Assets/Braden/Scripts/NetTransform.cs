3519f5fa8221cd37ee752793e22f0955114e1516
MUKILTEO\1719263 <1719263@mukilteo.wednet.edu>
2025-03-05 09:46:05 -0800

    its glorp

--- Assets/Braden/Scripts/NetTransform.cs
+++ Assets/Braden/Scripts/NetTransform.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class NetTransform : NetworkBehaviour
 {
+    Rigidbody2D playerBody;
+
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        if (IsServer)
+        if (IsOwner)
         {
             float theta = Time.frameCount / 10.0f;
-            transform.position = new Vector3((float)Math.Cos(theta), 0.0f, (float)Math.Sin(theta));
+            playerBody.position = new Vector3((float)Math.Cos(theta), 0.0f, (float)Math.Sin(theta));
         }
     }
 }

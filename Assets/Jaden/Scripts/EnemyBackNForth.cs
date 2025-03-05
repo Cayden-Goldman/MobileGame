7375b291846bd4aa90ddb20759e0b776b49d1978
MUKILTEO\1717295 <1717295@mukilteo.wednet.edu>
2025-03-05 09:32:06 -0800

    Emeny

---
+++ Assets/Jaden/Scripts/EnemyBackNForth.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class EnemyBackNForth : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rigidbody2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody2.linearVelocity = new Vector2(speed, rigidbody2.linearVelocityY);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pipe"))
+            speed = -speed;
+    }
+}

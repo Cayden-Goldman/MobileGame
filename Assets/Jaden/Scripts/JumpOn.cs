82ebdde5ed89620dd9681b33ff4ca8b09a156a92
MUKILTEO\1717295 <1717295@mukilteo.wednet.edu>
2025-03-07 08:49:12 -0800

    stomp

--- Assets/Jaden/Scripts/JumpOn.cs
+++ Assets/Jaden/Scripts/JumpOn.cs
@@ -1,16 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class JumpOn : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool destroying;
+    Rigidbody2D rb;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.CompareTag("Enemy") && !destroying)
+        {
+            StartCoroutine(DestroyWait(collision.gameObject));
+        }
+    }
+    private IEnumerator DestroyWait(GameObject destroy)
+    {
+        destroying = true;
+        yield return new WaitForSeconds(.1f);
+        Destroy(destroy);
+        rb.linearVelocityY = 5;
+        destroying = false;
     }
 }
